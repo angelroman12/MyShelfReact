23e2b662a1bc44f37e4162de854bc5e2089e2e9a
“Andrei <romanangel95@yahoo.com>
2021-09-01 11:36:38 +0300

    home page

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( <div className="nav">
+        <h1>My shelf</h1>
+        <ul>
+            <li><a href="/">Cart</a></li>
+            <li><a href="/">Admin</a></li>            
+            <li><a href="/">Contact</a></li>            
+        </ul>
+    </div> );
+}
+ 
+export default Navbar;
